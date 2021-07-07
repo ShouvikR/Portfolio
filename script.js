6f0d03723d30cfec5f7445aa4bb145a4f7cb98ac
Shouvik Roy <shouvikshikhar@gmail.com>
2021-07-06 21:20:18 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+// Menu hide & show 
+
+const navMenu = document.getElementById('nav-menu');
+    navToggle = document.getElementById('nav-toggle');
+    navClose = document.getElementById('nav-close');
+
+    if (navToggle) {
+        navToggle.addEventListener('click', () => {
+            navMenu.classList.add('show-menu')
+        })
+    }
+
+    if (navClose) {
+        navClose.addEventListener('click', () => {
+            navMenu.classList.remove('show-menu')
+        })
+    }
+
+
+
+    // Remove menu on mobile screen
+    const navLink = document.querySelectorAll('.nav-link');
+
+    function linkAction() {
+      const navMenu = document.getElementById("nav-menu");
+      // When we click on each nav__link, we remove the show-menu class
+      navMenu.classList.remove('show-menu');
+    }
+    navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+
+    // Skills toggle
+
+    const skillsContent = document.getElementsByClassName('skills-content');
+    skillsHeader = document.querySelectorAll('.skills-header')
+
+    function toggleSkills() {
+        let itemClass = this.parentNode.className;
+
+        for(i = 0; i < skillsContent.length; i++) {
+            skillsContent[i].className = 'skills-content skills-close'
+        }
+
+        if(itemClass === 'skills-content skills-close') {
+            this.parentNode.className = 'skills-content skills-open'
+        }
+
+    }
+
+    skillsHeader.forEach((el) => {
+        el.addEventListener('click', toggleSkills)
+    })
