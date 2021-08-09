5a6debd13892f549cb4f9d75a10de9e75e88f124
ShouvikR <shouvikshikhar@gmail.com>
2021-08-09 01:35:04 -0400

    bug fixed

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,7 @@ let typed = new Typed(".typing", {
   strings: [
     "I am a Front-End Web Developer.",
     "I like to watch Animation movies,",
-    "enjoy learning new things & travelling.",
+    "enjoy learning new things and travelling.",
     "An optimistic person.",
     "And very passionate about CODING!!",
   ],
